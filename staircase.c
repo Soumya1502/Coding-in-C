315c49b8159b43a94038033d0d717d8be966b49d
Soumya1502 <53609909+Soumya1502@users.noreply.github.com>
2021-09-25 22:03:40 +0530

    Nested loop
    A C program to print a stairacse using 0s and 1s

---
+++ staircase.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter number of stairs-->");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            printf("01");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
